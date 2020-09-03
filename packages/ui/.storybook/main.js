e6e039402cf1bb572669d682126cce5589b366c2
Nenad Brcic <neno@neno.ch>
2020-09-03 14:49:09 +0200

    Temporary remove addons

--- packages/ui/.storybook/main.js
+++ packages/ui/.storybook/main.js
@@ -1,9 +1,12 @@
 module.exports = {
-  stories: ["../src/**/*.stories.js"],
-  presets: ["@storybook/preset-scss"],
-  addons: [
-    "@storybook/addon-a11y/register",
-    "@whitespace/storybook-addon-html/register",
-    "@storybook/addon-viewport/register",
-  ],
-}
+    stories: ['../src/**/*.stories.js'],
+    presets: ['@storybook/preset-scss'],
+    // addons: [
+    // //   '@storybook/addon-a11y/register',
+    // //   '@whitespace/storybook-addon-html/register',
+    // //   '@storybook/addon-actions/register',
+    // //   '@storybook/addon-knobs/register',
+    // //   '@storybook/addon-docs',
+    // //   '@storybook/addon-viewport/register',
+    // ],
+  };
