5efa9ef9fc1f8475d000dbd884dfdf42ace465ac
Nenad Brcic <neno@neno.ch>
2020-09-04 10:11:46 +0200

    Upgrade to Storybook 6

--- packages/ui/.storybook/main.js
+++ packages/ui/.storybook/main.js
@@ -1,12 +1,24 @@
+// module.exports = {
+//     stories: ['../src/**/*.stories.js'],
+//     presets: ['@storybook/preset-scss'],
+//     addons: [
+//       '@storybook/addon-a11y/register',
+//       '@whitespace/storybook-addon-html/register',
+//     //   '@storybook/addon-actions/register',
+//     //   '@storybook/addon-knobs/register',
+//     //   '@storybook/addon-docs',
+//       '@storybook/addon-viewport/register',
+//     ],
+//   };
 module.exports = {
-    stories: ['../src/**/*.stories.js'],
+    stories: [
+        "../src/**/*.stories.mdx",
+        "../src/**/*.stories.@(js|jsx|ts|tsx)"
+    ],
     presets: ['@storybook/preset-scss'],
-    // addons: [
-    // //   '@storybook/addon-a11y/register',
-    // //   '@whitespace/storybook-addon-html/register',
-    // //   '@storybook/addon-actions/register',
-    // //   '@storybook/addon-knobs/register',
-    // //   '@storybook/addon-docs',
-    // //   '@storybook/addon-viewport/register',
-    // ],
-  };
+    addons: [
+        "@storybook/addon-links",
+        "@storybook/addon-essentials",
+        "@whitespace/storybook-addon-html/register"
+    ]
+}
--- packages/ui/.storybook/preview.js
+++ packages/ui/.storybook/preview.js
@@ -1,9 +1,21 @@
+// import "../src/js/index.js"
+// import "../src/styles/preview.scss"
+
+// import { addDecorator } from "@storybook/html"
+// import { withA11y } from "@storybook/addon-a11y"
+// import { withHTML } from "@whitespace/storybook-addon-html/html"
+
+// addDecorator(withA11y)
+// addDecorator(withHTML)
+
 import "../src/js/index.js"
 import "../src/styles/preview.scss"
 
 import { addDecorator } from "@storybook/html"
-import { withA11y } from "@storybook/addon-a11y"
 import { withHTML } from "@whitespace/storybook-addon-html/html"
 
-addDecorator(withA11y)
 addDecorator(withHTML)
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+}
