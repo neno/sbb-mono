d8bcc2ee3e4ce73c732e80f12042e68e082d0e57
Josip Anic <josipanic1985@gmail.com>
2020-09-08 23:33:36 +0200

    Centering tab-list

--- packages/ui/src/02-molecules/tab-list/index.jsx
+++ packages/ui/src/02-molecules/tab-list/index.jsx
@@ -3,7 +3,7 @@ import Tab from '../../01-atoms/tab';
 
 const TabList = ({ label, tabs, isCentered }) => (
 
-    <div className={`tablist${isCentered ? ' u-text-center' : ''}`} role="tablist" aria-label={label}>
+    <div className={`m-tablist${isCentered ? ' u-text-center' : ''}`} role="tablist" aria-label={label}>
         {tabs.map(tab => (
             <Tab key={tab.id} id={tab.id} label={tab.label} active={tab.active} />
         ))}
