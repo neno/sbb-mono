c9a33aa1cf6317c4d668cf15a4a76a38f0ee0b51
Josip Anic <josipanic1985@gmail.com>
2020-09-09 11:30:05 +0200

    Adding content to login template

--- packages/ui/src/04-templates/login-template/index.jsx
+++ packages/ui/src/04-templates/login-template/index.jsx
@@ -24,16 +24,29 @@ const OverviewTemplate = () => (
         <div className="l-divider-big u-text-center">
             <h1>Login</h1>
         </div>
-        <Tabs tabs={tabs} tabsListCentered="true">
-            <TabPanel id="mitarbeitende" active="true">
-                <Button classes={['a-btn--primary a-btn--arrows']} arrows="true" url="/">
-                    Login
-                </Button>
-            </TabPanel>
-            <TabPanel id="pensionierte">
-                <LoginForm />
-            </TabPanel>
-        </Tabs>
+        <div className="l-divider-medium">
+            <Tabs tabs={tabs} tabsListCentered="true">
+                <TabPanel id="mitarbeitende" active="true">
+                    <div className="u-text-center">
+                        <Button classes={['a-btn--primary a-btn--arrows']} arrows="true" url="/">
+                            Login
+                        </Button>
+                    </div>
+                </TabPanel>
+                <TabPanel id="pensionierte">
+                    <div className="l-login-panel">
+                        <div className="u-text-center">
+                            <p>
+                                Pensionierte melden sich mit den gleichen
+                                Anmeldeninformationen an, die sie auch auf
+                                www.sbb.ch/benefit benutzen.
+                            </p>
+                        </div>
+                        <LoginForm />
+                    </div>
+                </TabPanel>
+            </Tabs>
+        </div>
 
         <div className="u-text-center">
             <p>Probleme beim Login?</p>
