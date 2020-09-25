8fe03d5792f880a40b9ad32489c226970d317810
Josip Anic <josipanic1985@gmail.com>
2020-09-25 20:56:10 +0200

    Fixing styling errors

--- packages/ui/src/02-molecules/teaser/index.tsx
+++ packages/ui/src/02-molecules/teaser/index.tsx
@@ -5,14 +5,14 @@ import Image, { ImageProps } from '../../01-atoms/image';
 interface Props {
     imageData: ImageProps;
     hasCTA: boolean;
-    href?: string;
+    href: string;
     classes?: string[];
     title?: string;
     text?: string;
 }
 
 const Teaser: React.FC<Props> = ({
-    imageData, title, text, href = '#', classes = [], hasCTA,
+    imageData, title, text, href, classes = [], hasCTA,
 }) => {
     const cls = hasCTA ? 'm-teaser m-teaser--cta' : 'm-teaser';
     return (
