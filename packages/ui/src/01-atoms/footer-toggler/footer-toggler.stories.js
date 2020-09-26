eb86218a78f101dcccebe43f36d2347427b5faa7
Josip Anic <josipanic1985@gmail.com>
2020-09-26 11:29:59 +0200

    Set Footer Toggle as Footer Toggler

--- packages/ui/src/01-atoms/footer-toggler/footer-toggler.stories.js
+++ packages/ui/src/01-atoms/footer-toggler/footer-toggler.stories.js
@@ -1,12 +1,12 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import { renderToStaticMarkup } from 'react-dom/server';
-import FooterToggle from './index';
+import FooterToggler from './index';
 
 export default {
-    title: '01-Atoms/Footer Toggle',
+    title: '01-Atoms/Footer Toggler',
 };
 
 export const Default = () => renderToStaticMarkup(
-    <FooterToggle controls="default" text="Some text" />,
+    <FooterToggler controls="default" text="Some text" />,
 );
