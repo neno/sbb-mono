daf7b01336c33c706b01dbe7a8ec6adf37f27a6b
Nenad Brcic <neno@neno.ch>
2020-09-28 10:53:14 +0200

    Reset storybook scripts and style import. Cleanup.

--- packages/ui/.storybook/preview.js
+++ packages/ui/.storybook/preview.js
@@ -1,6 +1,7 @@
 import { addDecorator } from "@storybook/html"
 import { withHTML } from "@whitespace/storybook-addon-html/html"
 
+import "../src/styles/index.scss";
 import "../src/js/index.js"
 
 addDecorator(withHTML)
